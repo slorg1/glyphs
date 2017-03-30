ddbb6d5a73199495edac308cd6d97490c8137d06
slorg1 <slorg1@gmail.com>
2017-03-30 15:57:14 -0400

    + might work

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='glyphs',
+      version='0.1',
+      description='Swiss army knife of data extraction',
+      author='slorg1',
+      url='https://github.com/slorg1/glyphs',
+      packages=find_packages(),
+      package_dir={'glyphs':'src/glyphs'},
+      install_requires=[
+            "six >= 1.10.1",
+        ],
+
+     )
