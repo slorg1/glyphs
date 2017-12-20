e4dbb6f967ca80db0616b8ad44616d65ee14de02
dev <dev@lovepop-dev>
2017-12-20 13:57:21 -0500

    + should fix missing package issue

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 
+from __future__ import unicode_literals
 from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='glyphs',
-      version='0.1',
+      version='0.1.1',
       description='Swiss army knife of data extraction',
       author='slorg1',
       url='https://github.com/slorg1/glyphs',
-      packages=[
-                'glyphs',
-                'glyphs.helpers',
-                'glyphs.ro',
-                'glyphs.rw',
-                'glyphs.utils',
-                ],
+      packages=find_packages('src'),
       package_dir={'':'src'},
       install_requires=[
             "six >= 1.10.0",
