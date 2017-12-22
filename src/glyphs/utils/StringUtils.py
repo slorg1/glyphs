fb04723d63a02653fadd9deb49cd63b43c197021
dev <slorg1@gmail.com>
2017-12-22 12:59:50 -0500

    + add new string utils method

--- src/glyphs/utils/StringUtils.py
+++ src/glyphs/utils/StringUtils.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+
 import six
 
 
@@ -16,6 +17,22 @@ class StringUtils(object):
             @postcondition: (value is None) == (return is None)
             @postcondition: return is None or isinstance(return, six.text_type)
         """
+
         return value if value is None else six.text_type(value)
+
+    @staticmethod
+    def to_unicode_not_none(value):
+        """
+            Returns the string representation of the given L{value} if it is not C{None}.
+
+            @rtype: six.text_type
+
+            @raise ValueError: raises a value error if the given L{value} is {None}
+        """
+
+        if value is None:
+            raise ValueError()
+
+        return six.text_type(value)
 
     __slots__ = tuple()
